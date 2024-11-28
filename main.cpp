9198bc7f165f42ba850b959dbcd1e9383e4edcb1
Dmytro Khodarevskyi <xkhoda01@vutbr.cz>
2024-11-28 19:46:29 +0100

    statistics + profitable settings

--- main.cpp
+++ main.cpp
@@ -37,45 +37,177 @@ using namespace std;
 Facility Slot("Slot");
 
 Histogram Test("Losses", 0, 100, 20);
-Histogram StartBalances("Start Balances", 100, 2000, 20);  // Histogram for start balances
-Histogram EndBalances("End Balances", 100, 2000, 20);  // Histogram for end balances
+Histogram StartBalances("Start Balances", 100, 2000, 20); // Histogram for start balances
+Histogram EndBalances("End Balances", 100, 2000, 20);     // Histogram for end balances
 
 double CasinoBalance = SLOT_BALANCE;
+
+class Statistics
+{
+
+public:
+    struct PlayerStats
+    {
+        double startBalance;
+        double endBalance;
+        double timePlayed;
+
+        int losses;
+        int returns;
+        int wins;
+        int bigWins;
+
+        int spins;
+    };
+
+    vector<PlayerStats> playerStats;
+
+    void AddPlayerStats(double startBalance,
+                        double endBalance,
+                        double timePlayed,
+                        int spins,
+                        int losses,
+                        int returns,
+                        int wins,
+                        int bigWins)
+    {
+        PlayerStats stats;
+        stats.startBalance = startBalance;
+        stats.endBalance = endBalance;
+        stats.timePlayed = timePlayed;
+        stats.spins = spins;
+        stats.losses = losses;
+        stats.returns = returns;
+        stats.wins = wins;
+        stats.bigWins = bigWins;
+        playerStats.push_back(stats);
+    }
+
+    void PrintMeanStats()
+    {
+        double totalStartBalance = 0;
+        double totalEndBalance = 0;
+        double totalTimePlayed = 0;
+        int totalSpins = 0;
+        int totalLosses = 0;
+        int totalReturns = 0;
+        int totalWins = 0;
+        int totalBigWins = 0;
+
+        for (unsigned int i = 0; i < playerStats.size(); i++)
+        {
+            totalStartBalance += playerStats[i].startBalance;
+            totalEndBalance += playerStats[i].endBalance;
+            totalTimePlayed += playerStats[i].timePlayed;
+            totalSpins += playerStats[i].spins;
+            totalLosses += playerStats[i].losses;
+            totalReturns += playerStats[i].returns;
+            totalWins += playerStats[i].wins;
+            totalBigWins += playerStats[i].bigWins;
+        }
+
+        double winrate = static_cast<double>(totalWins + totalBigWins) / totalSpins;
+        double meanWinrate = winrate / static_cast<double>(playerStats.size());
+        double meanLosses = static_cast<double>(totalLosses) / playerStats.size();
+        double meanReturns = static_cast<double>(totalReturns) / playerStats.size();
+        double meanWins = static_cast<double>(totalWins) / playerStats.size();
+        double meanBigWins = static_cast<double>(totalBigWins) / playerStats.size();
+        double meanTimePlayed = static_cast<double>(totalTimePlayed) / playerStats.size();
+        double meanSpins = static_cast<double>(totalSpins) / playerStats.size();
+        double meanStartBalance = static_cast<double>(totalStartBalance) / playerStats.size();
+        double meanEndBalance = static_cast<double>(totalEndBalance) / playerStats.size();
+
+        double probabilities[] = {NO_WIN_SPIN, COMEBACK_SPIN, SMALL_WIN_SPIN, BIG_WIN_SPIN};
+        double meanWinMultiplier = MAX_SMALL_WIN - MIN_SMALL_WIN / 2;
+        double meanJackpotMultiplier = MAX_JACKPOT_MULTIPLIER - MIN_JACKPOT_MULTIPLIER / 2;
+
+        double multipliers[] = {0, 1, meanWinMultiplier, meanJackpotMultiplier};
+
+        int num_outcomes = sizeof(probabilities) / sizeof(probabilities[0]);
+        
+        double rtp = 0.0;
+
+        // Iterate through all outcomes to calculate weighted payout
+        for (int i = 0; i < num_outcomes; i++)
+        {
+            rtp += probabilities[i] * multipliers[i];
+        }
+
+        double rtp_percentage = rtp * 100;
+
+        cout << endl
+             << "+========================================+" << endl;
+        cout << "Total players: " << playerStats.size() << endl;
+        cout << "Total spins: " << totalSpins << endl;
+        cout << "Total losses: " << totalLosses << endl;
+        cout << "Total returns: " << totalReturns << endl;
+        cout << "Total wins: " << totalWins << endl;
+        cout << "Total big wins: " << totalBigWins << endl;
+        cout << "+========================================+" << endl;
+        cout << "Mean start balance: " << meanStartBalance << " dollars." << endl;
+        cout << "Mean end balance: " << meanEndBalance << " dollars." << endl;
+        cout << "+========================================+" << endl;
+        cout << "Mean time played: " << meanTimePlayed << " seconds." << endl;
+        cout << "+========================================+" << endl;
+        cout << "Mean spins: " << meanSpins << endl;
+        cout << "+========================================+" << endl;
+        cout << "Mean losses: " << meanLosses << endl;
+        cout << "Mean returns: " << meanReturns << endl;
+        cout << "Mean wins: " << meanWins << endl;
+        cout << "Mean big wins: " << meanBigWins << endl;
+        cout << "+========================================+" << endl;
+        cout << "Mean winrate: " << meanWinrate << endl;
+        cout << "+========================================+" << endl;
+        cout << "RTP: " << rtp_percentage << endl;
+        cout << "+========================================+" << endl;
+    }
+};
+
+Statistics stats;
 
 class Player : public Process
 {
     double EntryTime;
     double balance;
+
+    int spins = 0;
+    int losses = 0;
+    int returns = 0;
+    int wins = 0;
+    int bigWins = 0;
     void Behavior()
     {
         EntryTime = Time;
         Seize(Slot);
-        random_device rd;          // Obtain a random number from hardware
-        mt19937 gen(rd());         
-        uniform_real_distribution<double> dist(100.0, 1000.0); 
-        balance = dist(gen);      
+        random_device rd; // Obtain a random number from hardware
+        mt19937 gen(rd());
+        uniform_real_distribution<double> dist(100.0, 1000.0);
+        balance = dist(gen);
 
-        cout << "Player " << Name() << " starts with a balance of " << balance << " dollars." << endl;
+        double startBalance = balance;
+
+        // cout << "Player " << Name() << " starts with a balance of " << balance << " dollars." << endl;
         StartBalances(balance);
         if (balance < 100)
         {
-            cout << "Player " << Name() << " cannot play. Not enough money!" << endl;
+            // cout << "Player " << Name() << " cannot play. Not enough money!" << endl;
             Release(Slot);
-            return;  
+            return;
         }
-
 
         while (true)
         {
             if (balance < 5)
             {
-                cout << "Player " << Name() << " doesn't have enough balance to play. Going home at " << Time << endl;
+                // cout << "Player " << Name() << " doesn't have enough balance to play. Going home at " << Time << endl;
                 break; // End the game if the player has less than 5 dollars
             }
 
-            balance -= STAKE; // Deduct 5 dollars per spin
+            spins++;
+
+            balance -= STAKE;       // Deduct 5 dollars per spin
             CasinoBalance += STAKE; // Add 5 dollars to the casino balance
-            cout << "Player " << Name() << " spent 5 dollars for a spin. Remaining balance: " << balance << " dollars." << endl;
+            // cout << "Player " << Name() << " spent 5 dollars for a spin. Remaining balance: " << balance << " dollars." << endl;
 
             Wait(Exponential(5));
 
@@ -85,11 +217,13 @@ class Player : public Process
 
             if (outcome < NO_WIN_SPIN)
             {
-                cout << "Player " << Name() << " lost at " << Time << endl;
+                // cout << "Player " << Name() << " lost at " << Time << endl;
+
+                losses++;
 
                 if (respin < NO_WIN_RESPIN_CHANCE)
                 {
-                    cout << "Player " << Name() << " respinned at " << Time << endl;
+                    // cout << "Player " << Name() << " respinned at " << Time << endl;
                 }
                 else
                 {
@@ -98,12 +232,16 @@ class Player : public Process
             }
             else if (outcome < NO_WIN_SPIN + COMEBACK_SPIN)
             {
-                cout << "Player " << Name() << " won nothing at " << Time << endl;
+                // cout << "Player " << Name() << " won nothing at " << Time << endl;
+
                 balance += STAKE;
                 CasinoBalance -= STAKE;
+
+                returns++;
+
                 if (respin < COMEBACK_RESPIN_CHANCE)
                 {
-                    cout << "Player " << Name() << " respinned at " << Time << endl;
+                    // cout << "Player " << Name() << " respinned at " << Time << endl;
                 }
                 else
                 {
@@ -112,13 +250,17 @@ class Player : public Process
             }
             else if (outcome < NO_WIN_SPIN + COMEBACK_SPIN + SMALL_WIN_SPIN)
             {
-                cout << "Player " << Name() << " had a small win at " << Time << endl;
+                // cout << "Player " << Name() << " had a small win at " << Time << endl;
                 double smallWinMultiplier = MIN_SMALL_WIN + (Random() * (MAX_SMALL_WIN - MIN_SMALL_WIN));
+
                 balance += STAKE * smallWinMultiplier;
                 CasinoBalance -= STAKE * smallWinMultiplier;
+
+                wins++;
+
                 if (respin < SMALL_WIN_RESPIN_CHANCE)
                 {
-                    cout << "Player " << Name() << " respinned at " << Time << endl;
+                    // cout << "Player " << Name() << " respinned at " << Time << endl;
                 }
                 else
                 {
@@ -127,13 +269,17 @@ class Player : public Process
             }
             else
             {
-                cout << "Player " << Name() << " won a JACKPOT at " << Time << endl;
+                // cout << "Player " << Name() << " won a JACKPOT at " << Time << endl;
                 double jackpotMultiplier = MIN_JACKPOT_MULTIPLIER + (Random() * (MAX_JACKPOT_MULTIPLIER - MIN_JACKPOT_MULTIPLIER));
+
                 balance += STAKE * jackpotMultiplier;
                 CasinoBalance -= STAKE * jackpotMultiplier;
+
+                bigWins++;
+
                 if (respin < BIG_WIN_RESPIN_CHANCE)
                 {
-                    cout << "Player " << Name() << " respinned at " << Time << endl;
+                    // cout << "Player " << Name() << " respinned at " << Time << endl;
                 }
                 else
                 {
@@ -150,7 +296,11 @@ class Player : public Process
 
         cout << "Player " << Name() << " End balance " << balance << " dollars." << endl;
         EndBalances(balance);
-        Test(Time - EntryTime);
+        // Test(Time - EntryTime);
+
+        // cout << "WINS: " << wins << " BIG WINS: " << bigWins << " RETURNS: " << returns << " LOSSES: " << losses << endl;
+
+        stats.AddPlayerStats(startBalance, balance, Time - EntryTime, spins, losses, returns, wins, bigWins);
 
         Release(Slot);
     }
@@ -166,7 +316,7 @@ class PeopleGenerator : public Event
 };
 
 int main()
-{ 
+{
 
     // Initialize the random number generator with a time-based seed
     random_device rd;  // Obtain a random number from hardware
@@ -174,19 +324,21 @@ int main()
     RandomSeed(gen());
 
     // Set this generator to be used globally (if Random uses the global RNG)
-    srand(static_cast<unsigned int>(gen()));  // Seed the C++ random function
+    srand(static_cast<unsigned int>(gen())); // Seed the C++ random function
 
     Print("KAZIK\n");
     SetOutput("kazik.out");
     Init(0, RUNTIME);                  // experiment initialization for time 0..1000
     (new PeopleGenerator)->Activate(); // customer generator
     Run();                             // simulation
-    //Test.Output();                     // print of results
-    // Slot.Output();
+    // Test.Output();                     // print of results
+    //  Slot.Output();
     StartBalances.Output();
     EndBalances.Output();
     //   Box.Output();              // print of results
     //   Table.Output();
+
+    stats.PrintMeanStats();
 
     cout << "Casino balance: " << CasinoBalance << " dollars." << endl;
     return 0;
--- main.hpp
+++ main.hpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <simlib.h>
 
-#define RUNTIME 60 * 60 * 24 
+#define RUNTIME 60 * 60 * 24 * 7 * 4
 
 #define SLOT_BALANCE 10000
 
 #define MIN_DEPOSIT 100
-#define STAKE 5
+#define STAKE 10
 
-#define NO_WIN_SPIN 0.5
-#define COMEBACK_SPIN 0.3
-#define SMALL_WIN_SPIN 0.18
-#define BIG_WIN_SPIN 0.02 
+#define NO_WIN_SPIN 0.6
+#define COMEBACK_SPIN 0.21 
+#define SMALL_WIN_SPIN 0.18 
+#define BIG_WIN_SPIN 0.01
 
-#define NO_WIN_RESPIN_CHANCE 0.7
+#define NO_WIN_RESPIN_CHANCE 0.8
 #define COMEBACK_RESPIN_CHANCE 0.95
-#define SMALL_WIN_RESPIN_CHANCE 0.4
-#define BIG_WIN_RESPIN_CHANCE 0.1
+#define SMALL_WIN_RESPIN_CHANCE 0.8
+#define BIG_WIN_RESPIN_CHANCE 0.2
 
-#define MIN_JACKPOT_MULTIPLIER 100
-#define MAX_JACKPOT_MULTIPLIER 1000
+#define MIN_JACKPOT_MULTIPLIER 10
+#define MAX_JACKPOT_MULTIPLIER 20
 
-#define MIN_SMALL_WIN 3
-#define MAX_SMALL_WIN 10
+#define MIN_SMALL_WIN 2
+#define MAX_SMALL_WIN 4
 
 // enum SPIN_RESULT {
 //     NO_WIN,
@@ -30,4 +30,3 @@
 //     SMALL_WIN,
 //     BIG_WIN
 // };
-
